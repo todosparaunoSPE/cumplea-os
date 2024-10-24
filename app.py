8be5cd68b48a7e6edfd9b5d355f33f0636f6d518
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-10-24 08:09:57 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 08:07:35 2024
+
+@author: jperezr
+"""
+
+import streamlit as st
+
+# Título
+st.title("🎉 ¡Feliz cumpleaños, Rafaela! 🎉")
+
+# Mensaje
+mensaje = """
+Hoy celebro no solo un año más de vida, sino la maravillosa persona que eres. 
+Gracias por tu alegría, tu energía y todo lo que aportas a quienes te rodean. 
+Que este día esté lleno de momentos felices, y que el año que comienza traiga consigo mucha salud, amor, y nuevas aventuras.
+
+Te deseo lo mejor hoy y siempre.
+
+Con todo mi cariño,  
+Javier
+"""
+
+# Mostrar el mensaje
+st.write(mensaje)
+
+# Incluir el video de YouTube
+st.video("https://www.youtube.com/watch?v=a1Q6Zg8LPM4")
